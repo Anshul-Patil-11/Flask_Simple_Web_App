f10093d5d26bc5e7502cbc17e1ef015080ba7c4e
Anshul Patil <anshulpatil619@gmail.com>
2025-09-25 19:02:29 +0100

    Refactor welcome route and add additional routes for greeting, addition, squaring a number, and displaying a path

--- app.py
+++ app.py
@@ -5,8 +5,27 @@ app = Flask(__name__)
 
 # define root for the root url ("/")
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def welcome():
+    return 'Welcome to my Flask App!'
+
+# String Route
+@app.route('/greet/<name>')
+def greet(name):
+    return f'Hello {name}!'
+
+# Add Route
+@app.route('/add/<int:num1>/<int:num2>')
+def add(num1,num2):
+    return str(num1+num2)
+
+# Float Route
+@app.route('/square/<float:num>')
+def square(num):
+    return f'The square of {num} is {str(num*num)}'
+
+@app.route('/showpath/<path:subpath>')
+def showpath(subpath):
+    return f'The path you have entered is {subpath}'
 
 if __name__=='__main__':
     app.run(debug=True)
