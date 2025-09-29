53a8e9646a5354dc679251307f33abe9281a1da8
Anshul Patil <anshulpatil619@gmail.com>
2025-09-29 15:53:10 +0100

    Enhance Flask app by adding query handling and improving route comments

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for, request
 
 # flask app instance
 app = Flask(__name__)
 
 # define root for the root url ("/")
 @app.route('/')
-def welcome():
+def welcome():  # view function or endpoint
     return 'Welcome to my Flask App!'
 
 # String Route
-@app.route('/greet/<name>')
+@app.route('/greet/<name>') # Dynamic URL
 def greet(name):
     return f'Hello {name}!'
 
@@ -23,9 +23,27 @@ def add(num1,num2):
 def square(num):
     return f'The square of {num} is {str(num*num)}'
 
+# Path ROute
 @app.route('/showpath/<path:subpath>')
 def showpath(subpath):
     return f'The path you have entered is {subpath}'
+
+# URL For
+with app.test_request_context():
+    print(url_for('greet',name='Anshul'))
+
+@app.route('/query', methods=['GET'])
+def query_example():
+    # Accessing URL arguments
+    name = request.args.get('name')
+    age = request.args.get('age')
+
+    if name and age:
+        return f"Hello {name}, you are {age} years old."
+    else:
+        return "Please provide both name and age in the query string."
+
+    # works with this url http://127.0.0.1:5000/query?name=Anshul&age=23
 
 if __name__=='__main__':
     app.run(debug=True)
